a0da59b4a1734d11937e88bd87f84c04bd0194a1
VAKTAWAR <89545212+VAKTAWAR@users.noreply.github.com>
2021-09-14 00:59:06 +0530

    Add files via upload

---
+++ SQL Database creting.sql
@@ -0,0 +1,14 @@
+#lord Ram is great
+
+#creating database 
+create database mydb;
+use mydb;
+
+
+
+Create table student_info(ID integer,firstname varchar(20));
+
+insert into student_info value (1,'tessy');
+
+#view table
+select* from  student_info;
